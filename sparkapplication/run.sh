6ab7a7201df5e2a97a1488af725d405de6d014b0
aeg <aeg@amazon.com>
2018-03-07 18:57:21 +1100

    Add locust data to mock

---
+++ sparkapplication/run.sh
@@ -0,0 +1,1 @@
+spark-submit  --packages org.apache.spark:spark-streaming-kinesis-asl_2.11:2.1.2 kinesis_consumer.py myStream myStream https://kinesis.us-east-1.amazonaws.com us-east-1
---
+++ sparkapplication/tests/LocustTask.py
@@ -0,0 +1,18 @@
+from locust import Locust
+
+
+from locust import TaskSet, task
+
+
+class MockKinesisProducer(TaskSet):
+
+    @task
+    def put_data(self):
+        # kinesis = kinesis.connect_to_region("eu-west-1")
+        print("Hi.....")
+
+
+class MyLocust(Locust):
+    task_set = MockKinesisProducer
+    min_wait = 5000
+    max_wait = 15000

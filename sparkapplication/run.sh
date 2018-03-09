21823315a9b1c37a6adac95d55b4828721c5c2de
aeg <aeg@amazon.com>
2018-03-09 11:18:12 +1100

    Fix run.sh to point to the main.py

--- sparkapplication/run.sh
+++ sparkapplication/run.sh
@@ -1,4 +1,4 @@
-spark-submit  --packages org.apache.spark:spark-streaming-kinesis-asl_2.11:2.1.2 kinesis_consumer.py myStream myStream https://kinesis.us-east-1.amazonaws.com us-east-1
+spark-submit  --packages org.apache.spark:spark-streaming-kinesis-asl_2.11:2.1.2 main.py myStream myStream https://kinesis.us-east-1.amazonaws.com us-east-1
 
 # Kick off locut to data inot kinese
 locust -f MockKinesisProducerLocust.py --no-web -c 1 -r 1

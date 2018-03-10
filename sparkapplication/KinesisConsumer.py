a2b9455051569edaa500375e2f2dff2556a2c0b3
aeg <aeg@amazon.com>
2018-03-10 17:36:25 +1100

    Add log level

--- sparkapplication/KinesisConsumer.py
+++ sparkapplication/KinesisConsumer.py
@@ -8,6 +8,10 @@ from sparkapplication.CloudTrailLogProcessor import CloudTrailLogProcessor
 class KinesisConsumer:
     def run(self, appName, streamName, endpointUrl, regionName):
         sc = SparkContext(appName="PythonStreamingKinesisWordCountAsl")
+        print("Initialised SC")
+        #TODO: log warn and above only
+        logger = sc._jvm.org.apache.log4j
+        logger.LogManager.getLogger("org").setLevel(logger.Level.WARN)
         ssc = StreamingContext(sc, 1)
         dstreamRecords = KinesisUtils.createStream(
             ssc, appName, streamName, endpointUrl, regionName, InitialPositionInStream.LATEST, 2)

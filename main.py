3b3a1cc48b3d68ddc0b358a4722129810b7f5a89
aeg <aeg@amazon.com>
2018-03-13 20:58:20 +1100

    Fully parameterise names

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import sys
 from sparkapplication.KinesisConsumer import KinesisConsumer
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
+    if len(sys.argv) != 7:
         print(
-            "Usage: main.py <app-name> <stream-name> <endpoint-url> <region-name>",
+            "Usage: main.py <app-name> <stream-name> <endpoint-url> <region-name> <anomaly-stream-name> <rewrite-stream-name>",
             file=sys.stderr)
         sys.exit(-1)
     appName, streamName, endpointUrl, regionName, anomaly_stream_name, rewrite_steam_name= sys.argv[1:]

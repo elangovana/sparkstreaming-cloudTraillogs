1d76f94f208e5b2417677e8bd87a368fab19d397
aeg <aeg@amazon.com>
2018-03-14 13:38:33 +1100

    Create emr cluster

--- deploymentscript.py
+++ deploymentscript.py
@@ -53,3 +53,4 @@ client.create_stream(
 
 # start up scripts
 #sudo pip-3.4 install boto3
+#aws emr create-cluster --applications Name=Ganglia Name=Spark Name=Zeppelin --ec2-attributes '{"KeyName":"SparkNViginia","InstanceProfile":"EMR_EC2_DefaultRole","SubnetId":"subnet-dcaff0f3","EmrManagedSlaveSecurityGroup":"sg-de9226a8","EmrManagedMasterSecurityGroup":"sg-ec9c289a"}' --service-role EMR_DefaultRole --enable-debugging --release-label emr-5.12.0 --log-uri 's3n://aws-logs-606965926739-us-east-1/elasticmapreduce/' --name 'My cluster new' --instance-groups '[{"InstanceCount":2,"InstanceGroupType":"CORE","InstanceType":"m3.xlarge","Name":"Core Instance Group"},{"InstanceCount":1,"InstanceGroupType":"MASTER","InstanceType":"m3.xlarge","Name":"Master Instance Group"}]' --configurations '[{"Classification":"spark","Properties":{"maximizeResourceAllocation":"true"},"Configurations":[]}]' --scale-down-behavior TERMINATE_AT_TASK_COMPLETION --region us-east-1
